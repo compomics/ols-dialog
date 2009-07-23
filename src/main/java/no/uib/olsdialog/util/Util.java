3dd7fa8bba4fad0fcdf97ff7507a4cd8b4bf2af6
harald.barsnes <harald.barsnes@3c1c4c6c-6e19-11de-b692-19bc0a66ba1d>
2009-07-23 12:49:47 +0000

    Initial uploading of the source.

---
+++ src/main/java/no/uib/olsdialog/util/Util.java
@@ -0,0 +1,23 @@
+package no.uib.olsdialog.util;
+
+/**
+ * Includes help methods that are used by the other classes.
+ * 
+ * @author  Harald Barsnes
+ * 
+ * Created April 2005
+ */
+public final class Util {
+
+    /**
+     * Makes sure that all writing to the ErrorLog has a uniform appearence.
+     *
+     * Writes the given String to the errorLog. 
+     * Adds date and time of entry.
+     *
+     * @param logEntry
+     */
+    public static void writeToErrorLog(String logEntry) {
+        System.out.println(new java.util.Date(System.currentTimeMillis()).toString() + ": " + logEntry);
+    }
+}
